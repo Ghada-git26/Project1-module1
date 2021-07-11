f437701db861ca2d3f40903e57fd4958b3988861
Ghada <ghadamannai9@gmail.com>
2021-07-12 00:05:26 +0200

    HTML and CSS progress

---
+++ game.js
@@ -0,0 +1,17 @@
+const petsArray = ['Aigle', 'Chouette', 'Faon', 'Hamster', 'Jaguar', 'Oie', 'Zèbre']
+const pokemons = [{
+        picture: "",
+        name: "Aigle",
+        id: 1,
+    },
+    {
+        picture: "https://cdn.pixabay.com/photo/2018/01/21/23/31/animal-3097792_1280.jpg",
+        id: 1,
+        name: "Chouette",
+    },
+    {
+        picture: "https://ironhack-pokeapi.herokuapp.com/img/003Venusaur.png",
+        id: 2,
+        name: "Venusaur",
+    }
+]
