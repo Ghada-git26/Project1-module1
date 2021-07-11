c7c93e5b4da2da8ea52cd6bbb0ae2827c51909a1
Ghada <ghadamannai9@gmail.com>
2021-07-11 19:57:58 +0200

    first commit

---
+++ chronometer.js
@@ -0,0 +1,42 @@
+class Chronometer {
+    constructor() {
+      this.currentTime = 120;
+      this.intervalId = null;
+    }
+  
+    start(callback) {
+      this.intervalId = setInterval(() => {
+        this.currentTime--;
+        if (callback){
+          callback();
+        }
+      }, 1000);
+    }
+  
+  
+    getMinutes() {
+      
+      let numbermin = this.currentTime / 60;
+      return ~~numbermin;
+  
+    }
+  
+    getSeconds() {
+      let numbersec = (this.currentTime % 60);
+      return ~~numbersec;
+    }
+    computeTwoDigitNumber(value) {
+        
+        return value.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+      }
+    
+      stop() {
+        
+        clearInterval(this.intervalId);
+      }
+    
+      reset() {
+        
+        this.currentTime = 0;
+      }
+    }    
